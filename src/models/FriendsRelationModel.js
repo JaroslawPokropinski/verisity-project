cd5b3aabdeb656d8930a75f699ddd57b400ca127
Piotr Berezowski <piotr.berezowski97@gmail.com>
2019-10-29 20:08:25 +0100

    Add FriendsRelation model definition

---
+++ src/models/FriendsRelationModel.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class FriendsRelation extends Sequelize.Model { }
+  FriendsRelation.init({
+    user: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    },
+    friend: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      },
+      onDelete: 'CASCADE'
+    },
+    isAccepted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'friends_relation'
+  });
+  return FriendsRelation;
+};
