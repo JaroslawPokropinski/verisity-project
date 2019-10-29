9592f89a716d17ace43a2f4edb66c675c04f76a2
Piotr Berezowski <piotr.berezowski97@gmail.com>
2019-10-30 00:34:45 +0100

    Add tests to inviteFriend and acceptInvitation methods

--- src/tests/userServiceTest.js
+++ src/tests/userServiceTest.js
@@ -8,13 +8,16 @@ describe("UserService", function () {
   describe("password encorder", function () {
     let database = null;
     let UserModel = null;
+    let FriendsRelationModel = null;
     let userService = null;
 
     beforeEach(() => {
       database = new Sequelize('sqlite::memory:', { logging: null });
       UserModel = database.import(path.join(__dirname, '..', 'models', 'UserModel'));
-      userService = new UserService(UserModel);
+      FriendsRelationModel = database.import(path.join(__dirname, '..', 'models', 'FriendsRelationModel'));
+      userService = new UserService(UserModel, FriendsRelationModel);
     })
+
     it("getUser should return admin", (done) => {
       database.sync()
         .then(() => userService.cryptPassword('adminadmin'))
@@ -57,6 +60,69 @@ describe("UserService", function () {
         .then(() => userService.addUser('test_user@example.com', 'test_user2', 'test password2'))
         .then(() => done(true))
         .catch(() => done());
+    });
+  });
+
+  describe("friends relation", function() {
+    let database = null;
+    let UserModel = null;
+    let FriendsRelationModel = null;
+    let userService = null;
+
+    beforeEach(() => {
+      database = new Sequelize('sqlite::memory:', { logging: null });
+      UserModel = database.import(path.join(__dirname, '..', 'models', 'UserModel'));
+      FriendsRelationModel = database.import(path.join(__dirname, '..', 'models', 'FriendsRelationModel'));
+      userService = new UserService(UserModel, FriendsRelationModel);
+    })
+
+    it("inviteFriend should send invitation from one user to another", (done) => {
+      database.sync()
+        .then(() => userService.addUser('asd@asd.pl', 'user1', 'user1'))
+        .then(() => userService.addUser('qwe@qwe.pl', 'user2', 'user2'))
+        .then(() => userService.inviteFriend('user1', 'user2'))
+        .then(() => {
+          let user1 = null;
+          let user2 = null;
+
+          userService._getUserId('user1')
+            .then((_id1) => user1 = _id1)
+            .then(() => userService._getUserId('user2'))
+            .then((_id2) => user2 = _id2)
+            .then(() => FriendsRelationModel.findOne({where: {user: user1, friend: user2}}))
+            .then((relation) => {
+              chai.expect(relation).to.not.equal(null);
+              chai.expect(relation.isAccepted).to.equal(false);
+            })
+            .then((result) => done(result))
+            .catch((err) => done(err));
+        })
+        .catch((err) => done(err));
+    });
+
+    it("acceptInvitation should accept invitation from one user to another", (done) => {
+      database.sync()
+      .then(() => userService.addUser('asd@asd.pl', 'user1', 'user1'))
+      .then(() => userService.addUser('qwe@qwe.pl', 'user2', 'user2'))
+      .then(() => userService.inviteFriend('user1', 'user2'))
+      .then(() => userService.acceptInvitation('user2', 'user1'))
+      .then(() => {
+        let user1 = null;
+        let user2 = null;
+
+        userService._getUserId('user1')
+          .then((_id1) => user1 = _id1)
+          .then(() => userService._getUserId('user2'))
+          .then((_id2) => user2 = _id2)
+          .then(() => FriendsRelationModel.findOne({where: {user: user1, friend: user2}}))
+          .then((relation) => {
+            chai.expect(relation).to.not.equal(null);
+            chai.expect(relation.isAccepted).to.equal(true);
+          })
+          .then((result) => done(result))
+          .catch((err) => done(err));
+      })
+      .catch((err) => done(err));
     });
   });
 });
